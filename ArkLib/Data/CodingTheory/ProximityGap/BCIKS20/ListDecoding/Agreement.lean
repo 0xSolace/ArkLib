a073bb161cd752f1fc42ce6bd077910ea027aec9
lalalune <shawmakesmagic@gmail.com>
2026-06-05 06:40:42 -0700

    Agreement: raise lint budget for Hensel wrappers

--- ArkLib/Data/CodingTheory/ProximityGap/BCIKS20/ListDecoding/Agreement.lean
+++ ArkLib/Data/CodingTheory/ProximityGap/BCIKS20/ListDecoding/Agreement.lean
@@ -8,7 +8,7 @@ Authors: Quang Dao, Katerina Hristova, Frantisek Silvasi, Julian Sutherland,
 import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.ListDecoding.RootClearing
 import ArkLib.Data.CodingTheory.ProximityGap.BCIKS20.HenselNumerator
 
-set_option linter.style.longFile 6100
+set_option linter.style.longFile 6300
 
 /-!
 # BCIKS20 list-decoding agreement compatibility module
