4bb5824773700af1cc9d48ffad4190f68bc2f505
lalalune <shawmakesmagic@gmail.com>
2026-06-07 15:07:09 -0700

    chore: refresh generated imports

--- ArkLib.lean
+++ ArkLib.lean
@@ -242,6 +242,7 @@ import ArkLib.Data.CodingTheory.ProximityGap.CS25CoveringWitness
 import ArkLib.Data.CodingTheory.ProximityGap.CS25JointProxBound
 import ArkLib.Data.CodingTheory.ProximityGap.CS25NonDegeneratePigeonhole
 import ArkLib.Data.CodingTheory.ProximityGap.CS25RSCoveredFraction
+import ArkLib.Data.CodingTheory.ProximityGap.CS25RSMinWeight
 import ArkLib.Data.CodingTheory.ProximityGap.CS25RSNearBound
 import ArkLib.Data.CodingTheory.ProximityGap.CS25ReedSolomonHighDist
 import ArkLib.Data.CodingTheory.ProximityGap.CS25SecondMomentAssembly
